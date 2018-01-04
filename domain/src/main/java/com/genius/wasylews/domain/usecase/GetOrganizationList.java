7afcb32d4484c4403b9f99af786574fbc1e7064e
Wasylews <ua.wasylews@gmail.com>
2018-01-04 20:46:52 +0200

    pass Single to presenter

--- domain/src/main/java/com/genius/wasylews/domain/usecase/GetOrganizationList.java
+++ domain/src/main/java/com/genius/wasylews/domain/usecase/GetOrganizationList.java
@@ -3,11 +3,13 @@ package com.genius.wasylews.domain.usecase;
 import com.genius.wasylews.domain.model.Organization;
 import com.genius.wasylews.domain.repository.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import io.reactivex.Single;
+
 
 @Singleton
 public class GetOrganizationList {
@@ -19,9 +21,7 @@ public class GetOrganizationList {
         mRepository = repository;
     }
 
-    public List<Organization> execute() {
-        return mRepository.getOrganizations()
-                .collect(ArrayList<Organization>::new, ArrayList::add)
-                .blockingGet();
+    public Single<List<Organization>> execute() {
+        return mRepository.getOrganizations();
     }
 }
