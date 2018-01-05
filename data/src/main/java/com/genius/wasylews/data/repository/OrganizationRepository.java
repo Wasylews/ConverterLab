2ef12e20829653cd03cc06791a0a38c31d985c31
Wasylews <ua.wasylews@gmail.com>
2018-01-05 12:57:15 +0200

    basic repository implementation

--- data/src/main/java/com/genius/wasylews/data/repository/OrganizationRepository.java
+++ data/src/main/java/com/genius/wasylews/data/repository/OrganizationRepository.java
@@ -1,31 +1,80 @@
 package com.genius.wasylews.data.repository;
 
+import android.util.Log;
+
+import com.genius.wasylews.data.database.model.OrganizationModel;
+import com.genius.wasylews.data.database.model.mapper.OrganizationCurrencyModelMapper;
+import com.genius.wasylews.data.database.model.mapper.OrganizationMapper;
 import com.genius.wasylews.data.net.RestService;
+import com.genius.wasylews.device.preferences.PreferencesManager;
 import com.genius.wasylews.domain.model.Organization;
 import com.genius.wasylews.domain.repository.Repository;
+import com.raizlabs.android.dbflow.rx2.language.RXSQLite;
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import java.util.Date;
+import java.util.List;
 
 import javax.inject.Inject;
 
-import io.reactivex.Flowable;
 import io.reactivex.Single;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 
 public class OrganizationRepository implements Repository {
 
     private final RestService mRestAdapter;
 
+    private final PreferencesManager mPreferencesManager;
+
+    private static final String LAST_DATE_KEY = "LAST_UPDATE_DATE_KEY";
+
+    private static final String TAG = OrganizationRepository.class.getSimpleName();
+
     @Inject
-    public OrganizationRepository(RestService adapter) {
+    public OrganizationRepository(RestService adapter, PreferencesManager manager) {
         mRestAdapter = adapter;
+        mPreferencesManager = manager;
     }
 
     @Override
-    public Flowable<Organization> getOrganizations() {
-        return mRestAdapter.getOrganizations();
+    public Single<List<Organization>> getOrganizations() {
+        return RXSQLite.rx(SQLite.select().from(OrganizationModel.class))
+                .queryList()
+                .doOnSubscribe(this::fetchOrganizations)
+                .toFlowable()
+                .flatMapIterable(organizationModels -> organizationModels)
+                .map(OrganizationMapper::transform)
+                .toList();
+    }
+
+    private void fetchOrganizations(Disposable disposable) {
+        Log.d(TAG, "Fetching from service");
+        mRestAdapter.getOrganizations()
+                .filter(serviceResponse -> {
+                    Date lastDate = mPreferencesManager.getDate(LAST_DATE_KEY);
+                    Date currentDate = serviceResponse.getDate();
+
+                    if (currentDate != null) {
+                        if (currentDate.after(lastDate)) {
+                            Log.d(TAG, "Update date");
+                            mPreferencesManager.putDate(LAST_DATE_KEY, currentDate);
+                            return true;
+                        }
+                    }
+                    return false;
+                })
+                .map(OrganizationCurrencyModelMapper::transform)
+                .toFlowable()
+                .flatMapIterable(organizationCurrencyModels -> organizationCurrencyModels)
+                .observeOn(Schedulers.io())
+                .subscribe(BaseModel::save);
     }
 
     @Override
-    public Single<Organization> getOrganization(int id) {
+    public Organization getOrganization(int id) {
         return null;
     }
 }
