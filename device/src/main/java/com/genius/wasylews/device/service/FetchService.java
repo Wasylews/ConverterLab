b426ccc01ccd93f15f3e43cb5ccd9f87378225b5
Wasylews <ua.wasylews@gmail.com>
2018-02-12 14:18:18 +0200

    setup alarm for service, add service injector

--- device/src/main/java/com/genius/wasylews/device/service/FetchService.java
+++ device/src/main/java/com/genius/wasylews/device/service/FetchService.java
@@ -1,31 +1,36 @@
 package com.genius.wasylews.device.service;
 
-import android.app.Service;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import com.genius.wasylews.domain.repository.Repository;
 
 import javax.inject.Inject;
 
-import dagger.android.AndroidInjection;
+import dagger.android.DaggerService;
 
-public class FetchService extends Service {
+public class FetchService extends DaggerService {
 
+    private static final String TAG = FetchService.class.getSimpleName();
     @Inject
     Repository repository;
 
-    @Override
-    public void onCreate() {
-        AndroidInjection.inject(this);
-        super.onCreate();
+    public static PendingIntent newIntent(Context context) {
+        Intent intent = new Intent(context, FetchService.class);
+
+        return PendingIntent.getService(context,
+                0, intent, 0);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        repository.fetchOrganizations();
-        return super.onStartCommand(intent, flags, startId);
+        Log.d(TAG, "Running service");
+        repository.fetchOrganizations().subscribe(this::stopSelf);
+        return START_STICKY;
     }
 
     @Override
